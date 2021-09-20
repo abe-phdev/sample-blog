962cac419678c94874ccb00f330a360266feece2
Raymond <raymond@philippinedev.com>
2021-09-20 17:54:49 +0800

    create a test for blogs controller

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Blogs", type: :request do
+  let!(:blog) { FactoryBot.create :blog }
+  let!(:user) { FactoryBot.create :user }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'allow user to view the list of blogs' do
+      get blogs_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'allows user to view a specific blog' do
+      get blog_url(blog)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /new' do
+    it 'redirects when the user is not logged in' do
+      get new_blog_path
+      expect(response.status).to eq 302
+    end
+
+    it 'returns a status of 200 if logged in' do
+      headers =  [user.email, user.password] 
+      get new_user_session_url, headers: headers
+      expect(response).to be_successful
+    end
   end
 end
