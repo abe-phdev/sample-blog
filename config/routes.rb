856a1d78c148dd98352b349b01be3dce8509cbb4
Raymond <raymond@philippinedev.com>
2021-09-20 17:54:14 +0800

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :blogs do
-    # post 'comments', to: 'comments#create'
     resources :comments, module: :blogs
   end
 
   resources :tweets do
     resources :comments, module: :tweets
-    # post 'comments', to: 'comments#create'
   end
 end
